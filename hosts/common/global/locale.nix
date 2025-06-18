5fe82ba9fbf6a9c1c2c96e52468a6ec917466478
emoriver <emoriver@live.it>
2025-06-18 07:02:02 +0200

    iniziata la struttura base a host-utente-moduli

---
+++ hosts/common/global/locale.nix
@@ -0,0 +1,15 @@
+{lib, ...}: {
+  i18n = {
+    defaultLocale = lib.mkDefault "en_US.UTF-8";
+    extraLocaleSettings = {
+      #LC_TIME = lib.mkDefault "pt_BR.UTF-8";
+    };
+    supportedLocales = lib.mkDefault [
+      "en_US.UTF-8/UTF-8"
+      #"pt_BR.UTF-8/UTF-8"
+    ];
+  };
+  #location.provider = "geoclue2";
+  time.timeZone = lib.mkDefault "Europe/Rome";
+  services.automatic-timezoned.enable = true;
+}
