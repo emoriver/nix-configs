9d30220490b2d469c9ace0e5f489c02a290daee8
emoriver <emoriver@live.it>
2025-06-22 21:41:32 +0200

    spostato ancora kde

--- home/emoriver/features/desktop/kde/default.nix
+++ home/emoriver/features/desktop/kde/default.nix
@@ -1,3 +1,49 @@
-{imports = [../common];}
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    #./deluge.nix
+    #./discord.nix
+    #./dragon.nix
+    #./firefox.nix
+    #./font.nix
+    #./gtk.nix
+    #./kdeconnect.nix
+    #./pavucontrol.nix
+    #./playerctl.nix
+    #./qt.nix
+    #./sublime-music.nix
+  ];
 
-# Put here all the WM KDE staff specific to this user!
+  # Put here all the WM KDE staff specific to this user!
+  home.packages = with pkgs;
+    [
+      kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+      kdePackages.kcalc # Calculator
+      kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+      kdePackages.kcolorchooser # A small utility to select a color
+      kdePackages.kolourpaint # Easy-to-use paint program
+      kdePackages.ksystemlog # KDE SystemLog Application
+      kdePackages.sddm-kcm # Configuration module for SDDM
+      kdiff3 # Compares and merges 2 or 3 files or directories
+      kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+      kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+      hardinfo2 # System information and benchmarks for Linux systems
+      haruna # Open source video player built with Qt/QML and libmpv
+      wayland-utils # Wayland utilities
+      wl-clipboard # Command-line copy/paste utilities for Wayland
+    ];
+
+/*
+  # Also sets org.freedesktop.appearance color-scheme
+  dconf.settings."org/gnome/desktop/interface".color-scheme =
+    if config.colorscheme.mode == "dark"
+    then "prefer-dark"
+    else if config.colorscheme.mode == "light"
+    then "prefer-light"
+    else "default";
+
+  xdg.portal.enable = true;
+*/
+}
