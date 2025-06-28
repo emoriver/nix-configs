e73675daad226cdaf39da0a4aeb0240fa270feec
emoriver <emoriver@live.it>
2025-06-28 21:47:40 +0200

    ancora font

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -4,8 +4,8 @@
   # List your module files here
   # my-module = import ./my-module.nix;
 
-/*  
   fonts = import ./fonts.nix;
+/*  
   monitors = import ./monitors.nix;
   oama = import ./oama.nix;
   pass-secret-service = import ./pass-secret-service.nix;
