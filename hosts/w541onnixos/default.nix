cba150ae9b47abdae21fc16c3673138795c4b4ed
emoriver <emoriver@live.it>
2025-07-14 12:20:46 +0200

    rete bridge per virt-manager

--- hosts/w541onnixos/default.nix
+++ hosts/w541onnixos/default.nix
@@ -89,21 +89,17 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
+  networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.br0.useDHCP = true;
+  networking.bridges = {
+    "br0" = {
+      interfaces = [ "enp0s25" ];
+    };
+  };
 
 
   # Configure keymap in X11
