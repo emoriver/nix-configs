2585e3ce354f393133de2071b91a0ea6627bc1e8
emoriver <emoriver@live.it>
2025-07-26 16:37:32 +0200

    -

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -3,4 +3,6 @@
 
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  programs.partition-manager.enable = true; # Optional: Enable partition manager for KDE Plasma 6
 }
